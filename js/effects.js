d4a3f71296ec812831444d0e304dcb97fad01387
Dora Chan <dora.l.chan@gmail.com>
2014-04-13 13:41:43 -0700

    Save

--- js/effects.js
+++ js/effects.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		location.reload(true);
 	});
 
-	$('#about').css({'marginTop': $('.pot').height()+150+'px'});
+	$('#about').css({'marginTop': $('.pot img').height()+150+'px'});
 	$('.btn-survey').css({'marginTop': $('.pot').height()+20+'px'});
 
     new WOW().init();
