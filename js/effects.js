5f9adf2f5e044a0791f6fa5f3cb8d8cf47ac4593
Dora Chan <dora.l.chan@gmail.com>
2014-04-13 13:55:17 -0700

    Save

--- js/effects.js
+++ js/effects.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    $(window).resize(function() {
+        location.reload(true);
+    });
+
     var $header = $('.pot img');
 	$('#about').css({'marginTop': $header.height()+150+'px'});
 	$('.btn-survey').css({'marginTop': $('.pot').height()+20+'px'});
@@ -116,9 +120,5 @@ $(document).ready(function(){
 	});
 
 	$('.screenshots a').nivoLightbox();
-
-    $(window).resize(function() {
-        location.reload(true);
-    });
 
 });
